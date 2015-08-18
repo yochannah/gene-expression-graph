a4f5e7804658276199f6b2d6560bb66c8f2ffefb
Yo Yehudi <yochannah@gmail.com>
2015-08-18 12:36:53 +0100

    added sidebar

--- js/ui.js
+++ js/ui.js
@@ -1,15 +1,21 @@
 var util = require('./util'),
-template = require('../template/basechart.html');
+template = require('../template/basechart.html'),
+sidebarTemplate = require('../template/sidebar.html');
 var ui = function(settings){
   this.settings = settings;
   var init = function(){
-    settings.parentElem.innerHTML = getTemplate();
+    settings.parentElem.innerHTML = template;
+    settings.statusBar = settings.parentElem.querySelector('.status');
+    util.removeClass(settings.statusBar,"loader");
+    settings.parentElem.appendChild(getSidebar());
     util.addClass(settings.parentElem, "gene-expression-atlas-diseases");
     settings.statusBar = settings.parentElem.querySelector('.status');
   },
-  getTemplate = function(){
-    return template;
-  }
+  getSidebar = function(){
+    var sidebar = document.createElement("div");
+    sidebar.innerHTML = sidebarTemplate;
+    return sidebar;
+  };
   return {init:init}
 };
 
