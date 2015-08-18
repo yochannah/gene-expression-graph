45a047f9c233e4166244beeda533e7625587bf1e
Yo Yehudi <yochannah@gmail.com>
2015-08-18 12:19:22 +0100

    data formatter in chartbuilder.js mostly works - see //todo note for bug

--- js/chartBuilder.js
+++ js/chartBuilder.js
@@ -1,15 +1,15 @@
 var jQuery  = require('jquery'),
 sorter      = require('./sorter');
 
-var geneExpressionAtlasDiseasesDisplayer = {};
-geneExpressionAtlasDiseasesDisplayer.settingsUpdated = function() {
+var exDisplayer = {};
+exDisplayer.settingsUpdated = function() {
   jQuery("#gene-expression-atlas-diseases div.settings input.update").removeClass('inactive');
 };
 
-geneExpressionAtlasDiseasesDisplayer.originalList = {};
-geneExpressionAtlasDiseasesDisplayer.newList = [];
-
-geneExpressionAtlasDiseasesDisplayer.prepareOriginalList = function(response){
+exDisplayer.originalList = {};
+exDisplayer.newList = [];
+exDisplayer.peaks = {"up": 0, "down": 0};
+exDisplayer.prepareOriginalList = function(response){
   var expressions = [], expression,
   sortBy = sorter();
   for(var i=0; i < response[0].atlasExpression.length; i++){
@@ -21,101 +21,38 @@ geneExpressionAtlasDiseasesDisplayer.prepareOriginalList = function(response){
         'tStatistic': expression.tStatistic
       }]
     });
-  }
-
-
-  geneExpressionAtlasDiseasesDisplayer.originalList = {
+  };
+  exDisplayer.originalList = {
     "byName":   sortBy.name(expressions),
     "byTStatistic": expressions.sort(sortBy.tStatistic),
     "byPValue": expressions.sort(sortBy.pValue)
   };
-  geneExpressionAtlasDiseasesDisplayer.peaks = {"up": 0, "down": 0};
+  exDisplayer.createPeaks();
+  exDisplayer.updateGlobalPeaks();
+  console.log('originalList',exDisplayer.originalList);
+  console.log('peaks',exDisplayer.peaks);
+};
 
-  console.log('originalList',geneExpressionAtlasDiseasesDisplayer.originalList);
+/**
+!!!!!!!
+TODO: buggy right now because the sort order is problematic. Should work when we fix that.
+!!!!!!!!!
+ **/
+exDisplayer.createPeaks = function() {
+  var down = exDisplayer.originalList.byTStatistic[0].expressions[0].tStatistic,
+  up = exDisplayer.originalList.byTStatistic[exDisplayer.originalList.byTStatistic.length-1].expressions[0].tStatistic;
+
+  exDisplayer.peaks.up = up;
+  exDisplayer.peaks.down = down;
+};
+
+/**
+ * Determines the max tStat peak and updates the peaks property
+ */
+exDisplayer.updateGlobalPeaks = function() {
+  exDisplayer.peaks.global =
+  (exDisplayer.peaks.up > Math.abs(exDisplayer.peaks.down)) ?
+  exDisplayer.peaks.up : Math.abs(exDisplayer.peaks.down);
 }
 
-
- // load Goog, create the initial bag from Java, determine max t-stat peak --%>
- //(function() {
-
-  // Java to JavaScript --%>
-
-
-
-/*
-  // ordered by organ part --%>
-  <c:forEach var="cellType" items="${expressions.byName}">
-    var expressions = new Array();
-    <c:set var="cell" value="${cellType.value}"/>
-    <c:forEach var="expression" items="${cell.values}">
-      var tStatistic = ${expression.tStatistic};
-      var expression = {
-        'pValue': ${expression.pValue},
-        'tStatistic': tStatistic
-      };
-      expressions.push(expression);
-
-      // figure out min/max scale --%>
-      if (tStatistic > 0) {
-        if (tStatistic > geneExpressionAtlasDiseasesDisplayer.peaks.up) {
-            geneExpressionAtlasDiseasesDisplayer.peaks.up = tStatistic;
-        }
-      } else {
-        if (tStatistic < geneExpressionAtlasDiseasesDisplayer.peaks.down) {
-            geneExpressionAtlasDiseasesDisplayer.peaks.down = tStatistic;
-        }
-      }
-    </c:forEach>
-
-    var expression = {
-      'condition': '${cellType.key}',
-      'expressions': expressions
-    };
-    geneExpressionAtlasDiseasesDisplayer.originalList.byName.push(expression);
-  </c:forEach>
-
-  // ordered by t-statistic --%>
-  <c:forEach var="cellType" items="${expressions.byTStatistic}">
-    var expressions = new Array();
-    <c:set var="cell" value="${cellType.value}"/>
-    <c:forEach var="expression" items="${cell.values}">
-      var expression = {
-        'pValue': ${expression.pValue},
-        'tStatistic': ${expression.tStatistic}
-      };
-      expressions.push(expression);
-    </c:forEach>
-
-    var expression = {
-      'condition': '${cellType.key}',
-      'expressions': expressions
-    };
-    geneExpressionAtlasDiseasesDisplayer.originalList.byTStatistic.push(expression);
-  </c:forEach>
-
-  // ordered by p-value --%>
-  <c:forEach var="cellType" items="${expressions.byPValue}">
-    var expressions = new Array();
-    <c:set var="cell" value="${cellType.value}"/>
-    <c:forEach var="expression" items="${cell.values}">
-      var expression = {
-        'pValue': ${expression.pValue},
-        'tStatistic': ${expression.tStatistic}
-      };
-      expressions.push(expression);
-    </c:forEach>
-
-    var expression = {
-      'condition': '${cellType.key}',
-      'expressions': expressions
-    };
-    geneExpressionAtlasDiseasesDisplayer.originalList.byPValue.push(expression);
-  </c:forEach>
-
-  // set global t-stat peak for slider --%>
-  geneExpressionAtlasDiseasesDisplayer.peaks.global =
-  (geneExpressionAtlasDiseasesDisplayer.peaks.up > Math.abs(geneExpressionAtlasDiseasesDisplayer.peaks.down)) ?
-  geneExpressionAtlasDiseasesDisplayer.peaks.up : Math.abs(geneExpressionAtlasDiseasesDisplayer.peaks.down);
- })();
-*/
-module.exports = geneExpressionAtlasDiseasesDisplayer;
+module.exports = exDisplayer;
